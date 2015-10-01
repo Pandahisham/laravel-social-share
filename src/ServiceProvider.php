7d67454f0e4bdf7789467b2c389d0db6885126c9
Tom Shafer <tj@tjshafer.com>
2015-09-30 20:30:14 -0700

    Initial

---
+++ src/ServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace DraperStudio\SocialShare;
+
+use Tshafer\ServiceProvider\ServiceProvider as BaseProvider;
+
+/**
+ * Class ServiceProvider.
+ */
+class ServiceProvider extends BaseProvider
+{
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton('social-share', function () {
+            return new Share();
+        });
+    }
+}
